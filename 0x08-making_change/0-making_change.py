1d86ab0019b09d5f54f480a2cc4138d8ad29b72c
Obatula Fuad <obatulafuad@gmail.com>
2024-11-29 00:18:09 +0100

    Implement code for Coin Change Problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def makeChange(coins, total):
+
+    if total <= 0:
+        return 0
+
+    arr = [float('inf')] * (total + 1)
+    arr[0] = 0
+
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                arr[i] = min(arr[i], arr[i - coin] + 1)
+
+    return arr[total] if arr[total] != float('inf') else -1
