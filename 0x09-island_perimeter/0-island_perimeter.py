f30c588611b8c484ebb9f819d0f91095dbf68222
Akinwalee <obatulafuad@gmail.com>
2024-12-05 18:05:28 +0000

    Fix bugs

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,12 +9,13 @@ def island_perimeter(grid):
     FInd the perimeter of an island
     """
     perimeter = 0
-    for i in range(1, len(grid) - 1):
-        for j in range(1, len(grid[i]) - 1):
+    w = len(grid[0])
+    for i in range(0, len(grid)):
+        for j in range(0, w):
             if grid[i][j] == 1:
-                perimeter += 1 if grid[i - 1][j] == 0 else 0
-                perimeter += 1 if grid[i + 1][j] == 0 else 0
-                perimeter += 1 if grid[i][j - 1] == 0 else 0
-                perimeter += 1 if grid[i][j + 1] == 0 else 0
+                perimeter += 1 if i == 0 or grid[i - 1][j] == 0 else 0
+                perimeter += 1 if i == n - 1 or grid[i + 1][j] == 0 else 0
+                perimeter += 1 if j == 0 or grid[i][j - 1] == 0 else 0
+                perimeter += 1 if j == w - 1 or grid[i][j + 1] == 0 else 0
 
     return perimeter
