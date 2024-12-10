3545f882082c9c70d4022595d361fb95827832f2
Akinwalee <obatulafuad@gmail.com>
2024-12-10 17:30:28 +0000

    Implement algorithm to determin the winner of the Prime Game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Determine the winner in a Prime number Game
+"""
+
+
+def isWinner(x, nums):
+    """
+    Return the winner
+    """
+    score = [0, 0]
+    for num in nums:
+        n = getPrimes(num)
+
+        if n % 2:
+            score[0] += 1
+        else:
+            score[1] += 1
+
+    if score[0] > score[1]:
+        return "Maria"
+    elif score[1] > score[0]:
+        return "Ben"
+
+    return None
+
+
+def getPrimes(num: int) -> int:
+    """
+    Get the number of prime number between 1 and num
+    """
+
+    primes = [True for i in range(num + 1)]
+    p = 2
+    while (p * p <= num):
+        if primes[p] is True:
+            for i in range(p * p, num + 1, p):
+                primes[i] = False
+
+        p += 1
+
+    count = 0
+    for p in primes:
+        if p:
+            count += 1
+
+    return count
